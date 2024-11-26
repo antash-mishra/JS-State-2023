69d93758fb58158e35de6c3180bf32638cd2a8e8
Antash <mishraantash34@gmail.com>
2024-11-26 08:59:33 +0000

    served framework data publicly

--- data-etl/index.py
+++ data-etl/index.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with your frontend origin
+    allow_origins=["*"],  # Replace with your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
