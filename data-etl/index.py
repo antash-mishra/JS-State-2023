04a3071d0d88ec95db41a174907fc9b3c4d8c7a4
Antash <mishraantash34@gmail.com>
2025-01-12 09:25:20 +0000

    Allowed origin and resolved dockerfile issue

--- data-etl/index.py
+++ data-etl/index.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Replace with your frontend origin
+    allow_origins=["https://statejs-2023.netlify.app"],  # Replace with your frontend origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
